7cb0849bdd18164a4881286a2b487c54b8d74c8a
sercel23 <ercelserap@gmail.com>
2022-05-23 23:50:48 +0300

    Add search google and download image

--- app/src/main/java/com/serapercel/android_ml2/MainActivity.kt
+++ app/src/main/java/com/serapercel/android_ml2/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.serapercel.android_ml2
 
 import android.app.Activity
-import android.app.Instrumentation
+import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.media.MediaActionSound
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -15,12 +19,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.serapercel.android_ml2.databinding.ActivityMainBinding
 import com.serapercel.android_ml2.ml.BirdsModel
 import org.tensorflow.lite.support.image.TensorImage
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -69,6 +73,18 @@ class MainActivity : AppCompatActivity() {
             } else {
                 requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
+        }
+
+        //to redirect user to google search for the scientific name
+        outputTextView.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=${outputTextView.text}"))
+            startActivity(intent)
+        }
+
+        //to download image when longPress on ImageView
+        imageView.setOnLongClickListener {
+            requestPermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            return@setOnLongClickListener true
         }
     }
 
@@ -140,5 +156,46 @@ class MainActivity : AppCompatActivity() {
 
         // Releases model resources if no longer used.
         birdsmodel.close()
+    }
+
+    //to download to device
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        isGranted: Boolean ->
+        if (isGranted){
+            AlertDialog.Builder(this).setTitle("Download Image?")
+                .setMessage("Do you want to download this image to your device?")
+                .setPositiveButton("Yes"){_, _ ->
+                    val drawable:BitmapDrawable = imageView.drawable as BitmapDrawable
+                    val bitmap = drawable.bitmap
+                    downloadImage(bitmap)
+                }
+                .setNegativeButton("No"){dialog, _ ->
+                    dialog.dismiss()
+                }.show()
+        }else{
+            Toast.makeText(this, "Please allow permission to download image", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //fun that takes a bitmap and store to user's device
+    private fun downloadImage(mBitmap:Bitmap):Uri?{
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "Birds_Images"+System.currentTimeMillis()/1000)
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        }
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        if (uri!=null){
+            contentResolver.insert(uri, contentValues)?.also {
+                contentResolver.openOutputStream(it).use { outputStream->
+                    if (!mBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)){
+                        throw IOException("Could'nt save the bitmap")
+                    }else{
+                        Toast.makeText(applicationContext, "Image Saved", Toast.LENGTH_LONG).show()
+                    }
+                }
+                return it
+            }
+        }
+        return null
     }
 }
