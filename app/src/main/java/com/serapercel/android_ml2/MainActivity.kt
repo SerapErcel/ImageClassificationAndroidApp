27fc5c0ec4e989f053a3a288329e3dbd31284af9
sercel23 <ercelserap@gmail.com>
2022-05-23 23:27:11 +0300

    Add permissions and generate output

--- app/src/main/java/com/serapercel/android_ml2/MainActivity.kt
+++ app/src/main/java/com/serapercel/android_ml2/MainActivity.kt
@@ -1,11 +1,144 @@
 package com.serapercel.android_ml2
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.serapercel.android_ml2.databinding.ActivityMainBinding
+import com.serapercel.android_ml2.ml.BirdsModel
+import org.tensorflow.lite.support.image.TensorImage
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageView: ImageView
+    private lateinit var button: Button
+    private lateinit var outputTextView: TextView
+    private var GALLERY_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        imageView = binding.imageView
+        button = binding.bntCaptureImage
+        outputTextView = binding.outputTextView
+        val buttonLoad = binding.btnLoadImage
+
+        button.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                takePicturePreview.launch(null)
+            } else {
+                requestPermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+        buttonLoad.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                intent.type = "image/*"
+                val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                onResult.launch(intent)
+            } else {
+                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    //request camera permission
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                takePicturePreview.launch(null)
+            } else {
+                Toast.makeText(this, "Permission Denied! Try Again.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    //launch camera and take picture
+    private val takePicturePreview =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap)
+                outputGenerator(bitmap)
+            }
+        }
+
+    //get image
+    private val onResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Log.i("TAG", "This is the result: ${result.data} ${result.resultCode}")
+            onResultRecived(GALLERY_REQUEST_CODE, result)
+        }
+
+    private fun onResultRecived(requestCode: Int, result: ActivityResult?) {
+        when (requestCode) {
+            GALLERY_REQUEST_CODE -> {
+                if (result?.resultCode == Activity.RESULT_OK) {
+                    result.data?.data?.let { uri ->
+                        Log.i("TAG", "onResultRecived: $uri")
+                        val bitmap =
+                            BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+                        imageView.setImageBitmap(bitmap)
+                        outputGenerator(bitmap)
+                    }
+                } else {
+                    Log.e("TAG", "onActivityResult: error in selecting image")
+                }
+            }
+        }
+    }
+
+    private fun outputGenerator(bitmap: Bitmap) {
+        //declaring tensorflow lite model veriable
+        val birdsmodel = BirdsModel.newInstance(this)
+
+        // Converting bitmap into tensorflow image
+        val newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val tfimage = TensorImage.fromBitmap(newBitmap)
+
+        // Process the image using trained model and sort it in descending order
+        val outputs = birdsmodel.process(tfimage)
+            .probabilityAsCategoryList.apply {
+                sortByDescending { it.score }
+            }
+
+        // Getting result having high probability
+        val highProbabilityOutput = outputs[0]
+
+        // Setting output text
+        outputTextView.text = highProbabilityOutput.label
+        Log.i("TAG", "outputGenerator: $highProbabilityOutput")
+
+
+        // Releases model resources if no longer used.
+        birdsmodel.close()
     }
 }
